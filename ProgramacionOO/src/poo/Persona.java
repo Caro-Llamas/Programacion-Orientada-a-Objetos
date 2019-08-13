105223078002663df0e002fc9ed8ac573fcb0ec0
usuario <usuario@Carolina>
2019-08-13 14:27:54 -0500

    - clase (Atributos y funciones, metodos

---
+++ ProgramacionOO/src/poo/Persona.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package poo;
+
+/**
+ *
+ * @author usuario
+ */
+public class Persona {
+    String nombre;
+    int edad;
+    double estatura;
+    double peso;
+    String sexo;
+    
+    public void saludar(){
+        System.out.println("Hola mundo...");
+    } 
+    
+}
---
+++ ProgramacionOO/src/programacionoo/ProgramacionOO.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package programacionoo;
+
+import poo.Persona;
+
+/**
+ *
+ * @author usuario
+ */
+public class ProgramacionOO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Persona caro = new Persona(); //New se refiere a un nuevo objeto 
+        System.out.println("Hey");
+    }
+    
+}
